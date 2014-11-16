a0155998fef070b774655980b0d7dc99122ae644
Ebe Rodriguez <ebetron@gmail.com>
2014-11-16 17:34:08 -0500

    Setup Angular routing, controllers for creating news items and comments

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-angular.module('fakeNews',[])
+angular.module('fakeNews',['ui.router'])
+
+.config([
+	'$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider.state('home',{
+			url:'/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		}).state('posts',{
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+		$urlRouterProvider.otherwise('home');
+	}])
 
 .factory('posts',[function(){
 	var o = {
@@ -20,7 +36,11 @@ angular.module('fakeNews',[])
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
@@ -28,6 +48,25 @@ angular.module('fakeNews',[])
 
 		$scope.incrementUpvotes = function(post){
 			post.upvotes += 1;
+		}
+	}
+])
+
+.controller('PostsCtrl',[
+	'$scope','$stateParams','posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+			if($scope.body === ''){
+				return;
+			}
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
 		}
 	}
 ]);
