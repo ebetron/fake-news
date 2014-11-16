0c95b074e005d543a74f454b6591263091508d72
Ebe Rodriguez <ebetron@gmail.com>
2014-11-16 11:51:17 -0500

    Created first angular service

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 angular.module('fakeNews',[])
 
+.factory('posts',[function(){
+	var o = {
+		posts: [{title:'hello', link: '', upvotes: 0}]
+	};
+	return o;
+}])
+
 .controller('MainCtrl',[
-	'$scope',
-	function($scope){
+	'$scope','posts',
+	function($scope, posts){
 		$scope.test = 'Hello World!';
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
-		];
+		$scope.posts = posts.posts;
 
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){
@@ -30,10 +31,3 @@ angular.module('fakeNews',[])
 		}
 	}
 ]);
-
-.factory('posts',[function(){
-	var o = {
-		posts: []
-	};
-	return o;
-}]);
