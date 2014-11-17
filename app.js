8f21795776f189969203bb718abd51569a1a2a1c
Ebe Rodriguez <ebetron@gmail.com>
2014-11-17 16:02:51 -0500

    Created model for comments, included it on our app

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+//connect to DB
 var mongoose = require('mongoose');
+moongoose.connect('mongodb://localhost/news');
+require('./models/Posts');
+require('./models/Comments')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-//connect to DB
-moongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
