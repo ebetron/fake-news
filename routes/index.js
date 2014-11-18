1745e9b1f0bc65da69a80ab3217e21b2c6825721
Ebe Rodriguez <ebetron@gmail.com>
2014-11-17 19:49:42 -0500

    Data Layer routes completed. Backend is done, now we just need to hook it up to the Angular Frontend

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/*================== POSTS ===================*/
+
 /*Retrieve Posts*/
 router.get('/posts', function(req, res, next){
 	Post.find(function(err, posts){
@@ -19,9 +21,11 @@ router.get('/posts', function(req, res, next){
 	});
 });
 
-/*Retrieve Single Post*/
-router.get('/posts/:post', function(req,res){
-	res.json(req.post);
+/*Retrieve Single Post along with corresponding comments*/
+router.get('/posts/:post', function(req,res, next){
+	req.post.populate('comments', function(err, post){
+		res.json(post);
+	});
 });
 
 /*Push Posts*/
@@ -52,13 +56,78 @@ router.param('post',function(req, res, next, id){
 	});
 });
 
-/*Increase Upvotes*/
+/*============= COMMENTS ===================*/
+
+/*Retrieve Comments*/
+router.get('/comments', function(req, res, next){
+	Comment.find(function(err, comments){
+		if(err){
+			return next(err);
+		}
+		res.json(comments);
+	});
+});
+
+/*Retrieve Single Comment*/
+router.get('/comments/:comment', function(req, res){
+	res.json(req.comment);
+});
+
+/*Push Comments*/
+router.post('/posts/:post/comments', function(req, res, next){
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save(function(err, comment){
+		if(err){
+			return next(err);
+		}
+		req.post.comments.push(comment);
+		req.post.save(function(err, post){
+			if(err){
+				return next(err);
+			}
+			res.json(comment);
+		});
+	});
+});
+
+/*Preload Comments*/
+router.param('comment', function(req, res, next, id){
+	var query = Comment.findById(id);
+
+	query.exec(function(err, post){
+		if(err){
+			return next(err);
+		}
+		if(!comment){
+			return next(new Erro("can't find comment"));
+		}
+		req.comment = post;
+		return next();
+	});
+});
+
+
+/*================ UPVOTES =====================*/
+
+/*Increase Upvotes on Posts*/
 router.put('/posts/:post/upvote', function(req, res, next){
 	req.post.upvote(function(err, post){
 		if(err){
 			return next(err);
 		}
 		res.json(post);
+	});
+});
+
+/*Increase Upvotes on Comments*/
+router.put('posts/:post/comments/:comment/upvote', function(req, res, next){
+	req.comment.upvote(function(err, comment){
+		if(err){
+			return next(err);
+		}
+		res.json(comment);
 	});
 });
 
